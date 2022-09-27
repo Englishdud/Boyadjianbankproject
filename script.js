d0092860bf9762c65df2aaaed60a7845c466397a
Englishdud <andrew_toronto@outlook.com>
2022-09-27 19:36:34 -0400

    Added everybody

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function submit(){
     user = document.getElementById('email').value;
     password = document.getElementById('password').value;
-    // console.log(user, password);
+    console.log(user, password);
     if(user == '510036553' && password == 'Andrew@5454'){
         window.location.href = 'Andrewbank.html';
     }
@@ -12,5 +12,65 @@ function submit(){
     }
     if(user == 'k3E&58mDne' && password == '79838'){
         window.location.href = 'Aadhilbank.html';
+    }
+    if(user == '$P!N6$BfyD' && password == '039414'){
+        window.location.href = 'Amybank.html';
+    }
+    if(user == 'Y3!Fc9F!Rm' && password == '039324'){
+        window.location.href = 'Ashleybank.html';
+    }
+    if(user == '&Q&$iM3n3M' && password == '967548'){
+        window.location.href = 'Calistobank.html';
+    }
+    if(user == '&hjpJ5J#RX' && password == '943287'){
+        window.location.href = 'Danielbank.html';
+    }
+    if(user == 'CLb7C$fzJ8' && password == '943777'){
+        window.location.href = 'Elissabank.html';
+    }
+    if(user == '6YH8$g4tbr' && password == '987312'){
+        window.location.href = 'Elizabethbank.html';
+    }
+    if(user == 'd36!$4Lgi!' && password == '108374'){
+        window.location.href = 'Emmanuelbank.html';
+    }
+    if(user == 'd36!$4Lgi!' && password == '983274'){
+        window.location.href = 'Fionabank.html';
+    }
+    if(user == '6cxSQ4@mbN' && password == '987612'){
+        window.location.href = 'Gabrielbank.html';
+    }
+    if(user == '@fAMxTPR36' && password == '098765'){
+        window.location.href = 'Madisonbank.html';
+    }
+    if(user == 'x6kzthX@Sr' && password == '567890'){
+        window.location.href = 'Maliabank.html';
+    }
+    if(user == 't!R!qh7NRT' && password == '456765'){
+        window.location.href = 'Matheusbank.html';
+    }
+    if(user == '?tyaSyESD7' && password == '865432'){
+        window.location.href = 'Nathanbank.html';
+    }
+    if(user == 'sFHP7cdG!#' && password == '475763'){
+        window.location.href = 'Nilabank.html';
+    }
+    if(user == '95X$t7KDXt' && password == '765213'){
+        window.location.href = 'Ruthbank.html';
+    }
+    if(user == 'gA!Ana9$i75' && password == '86223'){
+        window.location.href = 'Skylabank.html';
+    }
+    if(user == '3yHd3i!?6r' && password == '766766'){
+        window.location.href = 'Theabank.html';
+    }
+    if(user == 'oB7Lt&Q?F&' && password == '567567'){
+        window.location.href = 'Tylerbank.html';
+    }
+    if(user == '?4T73L6CxP' && password == '678912'){
+        window.location.href = 'Williambank.html';
+    }
+    if(user == '4o$TpqjbB&' && password == '761765'){
+        window.location.href = 'Zachbank.html';
     }
 }
