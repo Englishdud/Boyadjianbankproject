7219b3b38d5252d8d8851c9d6b054270715a392d
Englishdud <andrew_toronto@outlook.com>
2022-09-27 19:42:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function submit(){
     user = document.getElementById('email').value;
     password = document.getElementById('password').value;
     console.log(user, password);
-    if(user == '510036553' && password == 'Andrew@5454'){
+    if(user == '510036553' && password == 'Andrew5454'){
         window.location.href = 'Andrewbank.html';
     }
-    if(user == '510036554' && password == 'Aaron@5454'){
+    if(user == '510036554' && password == 'Aaron5454'){
         window.location.href = 'Aaronbank.html';
     }
     if(user == 'k3E&58mDne' && password == '79838'){
